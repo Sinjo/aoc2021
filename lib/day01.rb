474a7f17d6f000a37e9c357dc7b11d1869cb2f58
Chris Sinjakli <chris@sinjakli.co.uk>
2021-12-01 13:48:53 +0000

    Completed day 1

--- lib/day01.rb
+++ lib/day01.rb
@@ -1,5 +1,17 @@
 class Day01
   def part_a(input)
-    puts input
+    depths = input.lines.map { |i|
+      Integer(i)
+    }.each_cons(2).filter { |a, b|
+      b > a
+    }.count
+  end
+
+  def part_b(input)
+    depths = input.lines.map { |i|
+      Integer(i)
+    }.each_cons(3).each_cons(2).filter { |a, b|
+      b.sum > a.sum
+    }.count
   end
 end
